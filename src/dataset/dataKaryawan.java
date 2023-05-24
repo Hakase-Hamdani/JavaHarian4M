93a17f67c4db94c3222ce742d7746e7427d4674b
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-05-24 09:51:04 +0800

    komit kelima

--- src/dataset/dataKaryawan.java
+++ src/dataset/dataKaryawan.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dataset;
+import java.util.ArrayList;
 
 /**
  *
  * @author Administrator 10
  */
 public class dataKaryawan {
+    
+    private ArrayList<String> nip;
+    private ArrayList<String> nama;
+    private ArrayList<String> golongan;
+    private ArrayList<String> status;
+    private ArrayList<Integer> jumAnak;
+    private ArrayList<Integer> masaKerja;
+    
+    public dataKaryawan(){}; //konstruktor
+    
     
 }
