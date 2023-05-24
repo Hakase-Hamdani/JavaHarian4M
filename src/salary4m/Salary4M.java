fed1843f362785c2de9af97c8d9c31b6e3f20bf5
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-05-24 09:27:18 +0800

    komit kedua

--- src/salary4m/Salary4M.java
+++ src/salary4m/Salary4M.java
@@ -15,14 +15,35 @@ public class Salary4M {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String [] nama1 = {"A", "B"};
+        String nama2 [] = {"C", "D"};
+        String nama3 [] = new String[5];
+        
+        nama3[0] = "A";
+        nama3[1]="B";
+        nama3[2]="B";
+        nama3[3]="Anam3";
+        nama3[4]="B";
+        
+        int tahun [] = new int[5];
+        tahun[0]=2021;
+        tahun[1]=2022;
+        tahun[2]=2023;
+        tahun[3]=2024;
+        tahun[4]=2025;
+        
+        System.out.println(nama3.length);
+        System.out.println(tahun[4]);
+        //
+        
         // TODO code application logic here
-        karyawan myobj = new karyawan();
+        //karyawan myobj = new karyawan();
         //myobj.setGolongan("Golongan 3");
         //myobj.setMasaKerja(7);
         //myobj.setStatus("Menikah");
         //myobj.setTunjAnak(2);
         
-        System.out.println("Gaji Total "+ myobj.GajiTotal("Golongan 1", "Menikah", 3, 2));
+        //System.out.println("Gaji Total "+ myobj.GajiTotal("Golongan 1", "Menikah", 3, 2));
      
     }
     
